89afe0d6b4984f20311722d02e24c7dc35e7225a
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-08-23 18:38:06 -0400

    fixing changeset parser

--- lib/straw_hat/error/changeset_parser.ex
+++ lib/straw_hat/error/changeset_parser.ex
@@ -12,7 +12,7 @@ defmodule StrawHat.Error.ChangesetParser do
     metadata =
       opts
       |> tidy_opts()
-      |> Keyword.merge([field: field])
+      |> Keyword.merge([field_name: field])
 
     error_tuple
     |> get_code()
@@ -20,7 +20,7 @@ defmodule StrawHat.Error.ChangesetParser do
   end
 
   defp tidy_opts(opts) do
-    Keyword.drop(opts, [:validation, :max, :is, :min, :code])
+    Keyword.drop(opts, [:validation, :constraint])
   end
 
   defp get_code({message, opts}) do
@@ -28,36 +28,32 @@ defmodule StrawHat.Error.ChangesetParser do
       opts
       |> Enum.into(%{message: message})
       |> do_get_code()
-      |> Atom.to_string()
 
-    "ecto.validation." <> code
+    "ecto.changeset." <> code
   end
 
-  # Ecto.Changeset.validate_length/3 when the :is option fails validation
-  defp do_get_code(%{validation: :length, is: _}), do: :length_is
-
-  # Ecto.Changeset.validate_length/3 when the :min option fails validation
-  defp do_get_code(%{validation: :length, min: _}), do: :length_min
-
-  # Ecto.Changeset.validate_length/3 when the :max option fails validation
-  defp do_get_code(%{validation: :length, max: _}), do: :length_max
-
+  # @TODO: all this is wrong, waiting for PR to be merged
   defp do_get_code(%{validation: :number, message: message}) do
     cond do
       # Ecto.Changeset.validate_number/3 when the :less_than_or_equal_to option fails validation
-      String.contains?(message, "less than or equal to") -> :number_less_than_or_equal_to
+      String.contains?(message, "less than or equal to") ->
+        "validation.number.less_than_or_equal_to"
 
       # Ecto.Changeset.validate_number/3 when the :greater_than_or_equal_to option fails validation
-      String.contains?(message, "greater than or equal to") -> :number_greater_than_or_equal_to
+      String.contains?(message, "greater than or equal to") ->
+        "validation.number.greater_than_or_equal_to"
 
       # Ecto.Changeset.validate_number/3 when the :less_than option fails validation
-      String.contains?(message, "less than") -> :number_less_than
+      String.contains?(message, "less than") ->
+        "validation.number.less_than"
 
       # Ecto.Changeset.validate_number/3 when the :greater_than option fails validation
-      String.contains?(message, "greater than") -> :number_greater_than
+      String.contains?(message, "greater than") ->
+        "validation.number.greater_than"
 
       # Ecto.Changeset.validate_number/3 when the :equal_to option fails validation
-      String.contains?(message, "equal to") -> :number_equal_to
+      String.contains?(message, "equal to") ->
+        "validation.number.equal_to"
 
       true -> :unknown
     end
@@ -68,24 +64,37 @@ defmodule StrawHat.Error.ChangesetParser do
   # - Ecto.Changeset.put_assoc/3
   # - Ecto.Changeset.cast_embed/3
   # - Ecto.Changeset.put_embed/3
-  defp do_get_code(%{message: "is invalid", type: _}), do: :association
+  defp do_get_code(%{message: "is invalid", type: _}),
+    do: get_constraint_code("assoc") # @TODO: this is wrong, waiting for PR to be merged
 
   # Ecto.Changeset.unique_constraint/3
-  defp do_get_code(%{message: "has already been taken"}), do: :unique
+  defp do_get_code(%{message: "has already been taken"}),
+    do: get_constraint_code("unique")
 
   # Ecto.Changeset.foreign_key_constraint/3
-  defp do_get_code(%{message: "does not exist"}), do: :foreign
+  defp do_get_code(%{message: "does not exist"}),
+    do: get_constraint_code("foreign")
 
   # Ecto.Changeset.no_assoc_constraint/3
-  defp do_get_code(%{message: "is still associated with this entry"}), do: :no_assoc
+  defp do_get_code(%{message: "is still associated with this entry"}),
+    do: get_constraint_code("no_assoc")
 
-  defp do_get_code(%{validation: validation_name})
-    when is_atom(validation_name), do: validation_name
+  defp do_get_code(%{validation: validation_name}) when is_atom(validation_name),
+    do: validation_name |> Atom.to_string() |> get_validation_code()
+
+  defp do_get_code(%{constraint: constraint_name}) when is_atom(constraint_name),
+    do: constraint_name |> Atom.to_string() |> get_constraint_code()
 
   # Supplied when validation cannot be matched. This will also match
   # any custom errors added through
   # - Ecto.Changeset.add_error/4
   # - Ecto.Changeset.validate_change/3
   # - Ecto.Changeset.validate_change/4
-  defp do_get_code(_unknown), do: :unknown
+  defp do_get_code(_unknown), do: "unknown"
+
+  def get_validation_code(validation_name),
+    do: "validation." <> validation_name
+
+  def get_constraint_code(constraint_name),
+    do: "constraint." <> constraint_name
 end
