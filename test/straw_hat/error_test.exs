51a40c7aef13960edee90bd1d51ec803b0ac6864
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-08-23 22:38:04 -0400

    clean up test

--- test/straw_hat/error_test.exs
+++ test/straw_hat/error_test.exs
@@ -32,32 +32,32 @@ defmodule StrawHatTest.ErrorTest do
 
     {@default, @types}
     |> cast(params, type_keys)
+    |> validate_acceptance(:terms_of_service)
+    |> validate_change(:title, fn :title, title ->
+      if title == "foobar" do
+        [title: {"cannot be foo", [validation: :cant_be, value: "foobar"]}]
+      else
+        []
+      end
+    end)
+    |> validate_confirmation(:password)
+    |> validate_exclusion(:name, ~w(admin superadmin))
+    |> validate_format(:email, ~r/@/)
+    |> validate_inclusion(:age, 0..99)
+    |> validate_length(:title, is: 9)
+    |> validate_number(:age, less_than: 99)
+    |> validate_required(:missing_field)
   end
 
   describe "changetset" do
     test "get list of errors" do
-      changeset =
+      error_list =
         @params
         |> get_changeset()
-        |> validate_acceptance(:terms_of_service)
-        |> validate_change(:title, fn :title, title ->
-          if title == "foobar" do
-            [title: {"cannot be foo", [validation: :cant_be, value: "foobar"]}]
-          else
-            []
-          end
-        end)
-        |> validate_confirmation(:password)
-        |> validate_exclusion(:name, ~w(admin superadmin))
-        |> validate_format(:email, ~r/@/)
-        |> validate_inclusion(:age, 0..99)
-        |> validate_length(:title, is: 9)
-        |> validate_number(:age, less_than: 99)
-        |> validate_required(:missing_field)
+        |> StrawHat.Error.new()
+        |> IO.inspect()
 
-      error_list = StrawHat.Error.new(changeset)
-
-      assert error_list == :world
+      assert is_list(error_list)
     end
   end
 end
