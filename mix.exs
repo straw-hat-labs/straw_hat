7a4a8b17b7d3a98ca2711c20290ccdd4681a6102
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2017-09-28 21:16:20 -0400

    update deps

--- mix.exs
+++ mix.exs
@@ -53,10 +53,10 @@ defmodule StrawHat.Mixfile do
       {:poison, "~> 3.1"},
 
       # Tools
-      {:dialyxir, "~> 0.5", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
       {:credo, ">= 0.0.0", only: [:dev, :test], runtime: false},
-      {:excoveralls, ">= 0.0.0", only: :test, runtime: false},
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
+      {:excoveralls, ">= 0.0.0", only: [:test], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false}
     ]
   end
 
