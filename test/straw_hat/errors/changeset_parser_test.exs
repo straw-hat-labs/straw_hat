97b5fda06bb46b7dbe1d0fc2a71a9ad5650e5d92
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-11-03 01:34:31 -0700

    Add testing for changeset validations

--- test/straw_hat/errors/changeset_parser_test.exs
+++ test/straw_hat/errors/changeset_parser_test.exs
@@ -3,39 +3,131 @@ defmodule StrawHat.ErrorChangePaserTest do
   import Ecto.Changeset
   alias StrawHat.Error.ChangesetParser
 
-  @types %{
-    pets: {:array, :string},
-    title: :string,
-    password: :string,
-    password_confirmation: :string,
-    email: :string
-  }
+  defmodule Post do
+    use Ecto.Schema
 
-  @default %{title: "bar"}
-
-
-  @params %{
-    "pets" => ["pepehands"],
-    "title" => "foobar",
-    "password" => "123",
-    "password_confirmation" => "1234",
-    "email" => "invalid.com"
-  }
-
-  defp get_changeset(params) do
-    type_keys = Map.keys(@types)
-
-    {@default, @types}
-    |> cast(params, type_keys)
-    |> validate_confirmation(:password)
-    |> validate_length(:title, is: 9)
-    |> validate_subset(:pets, ["cat", "dog", "parrot"])
+    schema "posts" do
+      field :token, :integer, primary_key: true
+      field :title, :string, default: ""
+      field :body
+      field :uuid, :binary_id
+      field :color, :binary
+      field :decimal, :decimal
+      field :upvotes, :integer, default: 0
+      field :topics, {:array, :string}
+      field :virtual, :string, virtual: true
+      field :published_at, :naive_datetime
+      field :source, :map
+      field :permalink, :string, source: :url
+      belongs_to :category, Ecto.ChangesetTest.Category, source: :cat_id
+      has_many :comments, Ecto.ChangesetTest.Comment, on_replace: :delete
+      has_one :comment, Ecto.ChangesetTest.Comment
+    end
   end
 
-  test "parse/1 returns the correct error list" do
-    assert %StrawHat.Error{code: "something"} =
-      @params
-      |> get_changeset()
-      |> ChangesetParser.parse()
+
+  defp changeset(schema \\ %Post{}, params) do
+    cast(schema, params, ~w(id token title body upvotes decimal color topics virtual)a)
+  end
+
+  describe "parse/1" do
+    test "with validate_required/2" do
+      error =
+        %{}
+        |> changeset()
+        |> validate_required(:title)
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.required"
+    end
+
+    test "with validate_format/3" do
+      error =
+        %{"title" => "foobar"}
+        |> changeset()
+        |> validate_format(:title, ~r/@/)
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.format"
+    end
+
+    test "with validate_inclusion/3" do
+      error =
+        %{"title" => "hello"}
+        |> changeset()
+        |> validate_inclusion(:title, ~w(world))
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.inclusion"
+    end
+
+    test "with validate_subset/3" do
+      error =
+        %{"topics" => ["cat", "laptop"]}
+        |> changeset()
+        |> validate_subset(:topics, ~w(cat dog))
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.subset"
+    end
+
+    test "with validate_exclusion/3" do
+      error =
+        %{"title" => "world"}
+        |> changeset()
+        |> validate_exclusion(:title, ~w(world))
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.exclusion"
+    end
+
+    test "with validate_length/3" do
+      error =
+        %{"title" => "world"}
+        |> changeset()
+        |> validate_length(:title, min: 6)
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.length"
+    end
+
+    test "with validate_number/3" do
+      error =
+        %{"upvotes" => -1}
+        |> changeset()
+        |> validate_number(:upvotes, greater_than: 0)
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.number.greater_than"
+    end
+
+    test "with validate_confirmation/3" do
+      error =
+        %{"title" => "world", "title_confirmation" => nil}
+        |> changeset()
+        |> validate_confirmation(:title)
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.confirmation"
+    end
+
+    test "with validate_acceptance/3" do
+      error =
+        %{"terms_of_service" => "other"}
+        |> changeset()
+        |> validate_acceptance(:terms_of_service)
+        |> ChangesetParser.parse()
+        |> List.first()
+
+      assert error.code == "ecto.changeset.validation.acceptance"
+    end
   end
 end
