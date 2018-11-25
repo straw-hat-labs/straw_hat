12bddb269197761e7de717008f8e586a40f6a730
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-11-24 18:08:18 -0800

    Add spaces

--- lib/straw_hat/error/error_list.ex
+++ lib/straw_hat/error/error_list.ex
@@ -13,7 +13,7 @@ defmodule StrawHat.Error.ErrorList do
   defstruct errors: []
 
   @doc """
-  Creates a `t:StrawHat.Error.ErrorList.t/0`
+  Creates an `t:StrawHat.Error.ErrorList.t/0`
   """
   @since "0.4.0"
   @spec new([StrawHat.Error.t()]) :: t
@@ -23,10 +23,13 @@ end
 defimpl Enumerable, for: StrawHat.Error.ErrorList do
   @since "0.4.0"
   def reduce(error_list, acc, fun), do: Enumerable.reduce(error_list.errors, acc, fun)
+
   @since "0.4.0"
   def member?(error_list, element), do: {:ok, Enum.member?(error_list.errors, element)}
+
   @since "0.4.0"
   def count(error_list), do: {:ok, Enum.count(error_list.errors)}
+
   @since "0.4.0"
   def slice(_error_list), do: {:error, StrawHat.Error.ErrorList}
 end
