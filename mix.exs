81c3e715bd7b77e89c1354909e1334e0f4abc001
Yordis Prieto Lazo <yordis.prieto@gmail.com>
2018-11-24 19:09:27 -0800

    Versioning

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule StrawHat.MixProject do
   use Mix.Project
 
   @name :straw_hat
-  @version "0.4.5"
+  @version "0.4.6"
   @elixir_version "~> 1.7"
   @source_url "https://github.com/straw-hat-team/straw_hat"
 
